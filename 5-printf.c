0f2c69d3e1c142192b1769d74831e4f5cf176a6c
Ndapat <patiencejosephndani@gmail.com>
2025-10-26 23:20:10 -1100

    uses printf to output a code

---
+++ 5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Author: Joseph Patience Fortune
+ * Program: WinMingle Community C Training
+ * Description: Using the Printf function
+ */
+
+int main(void)
+{
+	//output with printf function
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return 0;
+}
