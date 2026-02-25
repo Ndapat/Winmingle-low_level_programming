7650caac005f6a0f4a52d04789e652de11550c41
Ndapat <fortunejoseph917@gmail.com>
2026-02-25 11:03:16 +0100

    prints a number either positive,negative or zero

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * Author: Patience Joseph
+ * Program: WinMingle Community C Training
+ * Description: Prints out positive,Negative numbers
+ */
+
+int main(void)
+{
+	srand(time(0));
+	int n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
