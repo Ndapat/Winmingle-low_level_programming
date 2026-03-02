612a9270d8e8ee5993aee97f40d909025e6107b9
Ndapat <patiencejosephndani@gmail.com>
2026-03-02 13:59:30 +0100

    prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Author: Patience Joseph
+ * Program: Winmingle Community C Training
+ * Des: print alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
