08e7d246c39ba33c85f65afe281a9c0db8b4180e
Ndapat <patiencejosephndani@gmail.com>
2026-03-02 15:58:13 +0100

    prints alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Author: Patience Joseph
+ * Program: Winmingle Community C Training
+ * Des: Prints ten times the alphabets in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	char letter;
+
+	while (count < 10)
+	{
+		for ( letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		count++;
+	}
+}
